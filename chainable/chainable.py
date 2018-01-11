aeb7e82421e178687b085b5221d5cdaf8ab1fe45
Oliver Rice <github@oliverrice.com>
2018-01-11 04:32:10 -0600

    enumerate, zip, collect, window

--- chainable/chainable.py
+++ chainable/chainable.py
@@ -1,5 +1,6 @@
-from typing import Iterable, Callable, Type
-from itertools import islice
+from collections import deque
+from typing import Callable, Collection, Iterable, Type 
+from itertools import islice, zip_longest
 
 __all__ = [
     'chainable'
@@ -11,6 +12,14 @@ class Chainable():
 
     def __init__(self, iterable: Iterable):
         self._iterable = iter(iterable)
+
+    def collect(self, n: int=None, container_type: Collection=list):
+        """Returns *n* values from iterable as type *container_type*
+
+        NOTE: Chainable.collect is not chainable. See Chainable.take
+        for chainable equivalent
+        """
+        return container_type(v for v in self.take(n))
 
     def map(self, func: Callable, *args, **kwargs):
         """Apply *func* to each element of iterable"""
@@ -26,6 +35,17 @@ class Chainable():
                 if func(v, *args, **kwargs):
                     yield v
         return Chainable(__imp())
+
+    def zip(self, iterable: Iterable):
+        """Yields tuples containing the i-th element from the i-th 
+        argument in the chainable, and the iterable"""
+        return Chainable(zip(self, iterable))
+
+    def enumerate(self, start: int=0):
+        """Yields tuples from the chainable where the first element
+        is a count from initial value *start*."""
+        return Chainable(enumerate(self, start=start))
+
 
     def take(self, n: int):
         """Yield first *n* items of the iterable"""
@@ -78,7 +98,47 @@ class Chainable():
 
         return Chainable(walk(self, level=0))
 
+    def window(self, n: int, step: int=1, fill_value: object=None):
+        """Yield a sliding window of width *n* over the given iterable.
 
+        Each window will advance in increments of *step*:
+
+        If the length of the iterable does not evenly divide by the *step* 
+        the final output is padded with *fill_value*
+        """
+        def _imp():
+            if n < 0:
+                raise ValueError('n must be >= 0')
+            if n == 0:
+                yield tuple()
+                return
+            if step < 1:
+                raise ValueError('step must be >= 1')
+
+            window = deque([], n)
+            append = window.append
+
+            # Initial deque fill
+            for _ in range(n):
+                append(next(self, fill_value))
+            yield tuple(window)
+
+            # Appending new items to the right causes old items to fall off the left
+            i = 0
+            for item in self:
+                append(item)
+                i = (i + 1) % step
+                if i % step == 0:
+                    yield tuple(window)
+
+            # If there are items from the iterable in the window, pad with the given
+            # value and emit them.
+            if (i % step) and (step - i < n):
+                for _ in range(step - i):
+                    append(fill_value)
+                yield tuple(window)
+
+        return Chainable(_imp())
 
     def __iter__(self):
          return self
--- tests/test_chainable.py
+++ tests/test_chainable.py
@@ -6,6 +6,11 @@ from chainable import chainable
 
 class TestChainable(unittest.TestCase):
 
+    def test_collect(self):
+        assert chainable(range(3)).collect() == [0, 1, 2]
+        assert chainable(range(3)).collect(container_type=tuple) == (0, 1, 2)
+        assert chainable(range(3)).collect(n=2) == [0, 1]
+ 
     def test_map(self):
         gen = chainable(range(3)).map(lambda x: x+2)
         collected = [x for x in gen]
@@ -34,6 +39,32 @@ class TestChainable(unittest.TestCase):
         gen = chainable(range(5))
         assert next(iter(gen)) == 0
 
+    def test_enumerate(self):
+        # Check default
+        gen = chainable(range(3)).enumerate()
+        assert gen.collect() == [(0, 0), (1, 1), (2, 2)]
+
+        # Check start param
+        gen = chainable(range(3)).enumerate(start=1)
+        assert gen.collect() == [(1, 0), (2, 1), (3, 2)]
+
+    def test_zip(self):
+        gen = chainable(range(3)).zip(range(3))
+        assert gen.collect() == [(0, 0), (1, 1), (2, 2)]
+    
+    def test_window(self):
+        # Check default
+        gen = chainable(range(5)).window(n=3)
+        assert gen.collect() == [(0, 1, 2), (1, 2, 3), (2, 3, 4)]
+
+        # Check step param
+        gen = chainable(range(5)).window(n=3, step=3)
+        assert gen.collect() == [(0, 1, 2), (3, 4, None)]
+
+        # Check fill_value param
+        gen = chainable(range(5)).window(n=3, step=3, fill_value='i')
+        assert gen.collect() == [(0, 1, 2), (3, 4, 'i')]
+
     def test_chainable(self):
         gen = chainable(count()) \
                 .map(lambda x: x**2) \
@@ -41,6 +72,10 @@ class TestChainable(unittest.TestCase):
                 .chunk(5) \
                 .take(3)
         assert next(gen) == [0, 267289, 1069156, 2405601, 4276624] 
+
+    def test_lazy(self):
+        #TODO(or)
+        pass
 
     def test_flatten(self):
         nested = [1, [2, (3, [4])], ['rbsd', 'abc'], (7,)]
