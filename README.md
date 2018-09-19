7c285b334939ec6f69b5df4ce18016a534dd3902
Oliver Rice <github@oliverrice.com>
2018-09-19 17:13:29 -0500

    explicit imports

--- README.md
+++ README.md
@@ -36,9 +36,9 @@ for item in pipeline:
   print(item)
 
 # Returns:
-# [0, 267289, 1069156, 2405601, 4276624] 
-# [6682225, 9622404, 13097161, 17106496, 21650409] 
-# [26728900, 32341969, 38489616, 45171841, 52388644] 
+# [0, 267289, 1069156, 2405601, 4276624]
+# [6682225, 9622404, 13097161, 17106496, 21650409]
+# [26728900, 32341969, 38489616, 45171841, 52388644]
 ```
 
 ### CLI
@@ -71,3 +71,18 @@ The following standard library modules are available to the flu command by defau
 ```walk_files(path='.') = recursively walk files starting at *path*```
 
 ```walk_dirs(path='.') = recursively walk directories starting at *path*```
+
+
+#### Examples:
+
+Path to all .txt files recursively, starting from current directly
+```
+flu 'walk_files().filter(lambda x: x.endswith(".txt"))'
+```
+
+First 100 prime numbers
+```
+flu 'flu(count(2)).filter(lambda z: flu(range(2, z-1)).filter(lambda x: z % x == 0).head(1) == []) \
+        .take(100)' -i itertools:count
+```
+
--- flupy/cli/cli.py
+++ flupy/cli/cli.py
@@ -4,8 +4,6 @@ import sys
 from signal import SIG_DFL, SIGPIPE, signal
 
 from flupy import flu, with_iter
-from flupy.cli.lazy_import import (collections, csv, itertools, json, math, os, random, re,
-                                   statistics)
 from flupy.cli.utils import walk_dirs, walk_files
 
 
@@ -14,16 +12,40 @@ def read_file(filename):
         yield from f
 
 
-parser = argparse.ArgumentParser(description="flupy: a fluent interface for python")
+parser = argparse.ArgumentParser(
+        description="flupy: a fluent interface for python",
+        formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument("command", help="flupy command to execute on input")
 parser.add_argument("-f", "--file", help="path to input file")
-
+parser.add_argument(
+        "-i",
+        "--import",
+        nargs="*",
+        default=[],
+        help="modules to import\n" \
+             "Syntax: <module>:<object>:<alias>\n" \
+             "Examples:\n" \
+             "\t'import os' = '-i os'\n" \
+             "\t'import os as op_sys' = '-i os::op_sys'\n" \
+             "\t'from os import environ' = '-i os:environ'\n" \
+             "\t'from os import environ as env' = '-i os:environ:env'\n"
+             )
 
 def main():
     args = parser.parse_args()
 
     _command = args.command
     _file = args.file
+    _import = getattr(args, 'import')
+
+    for imp_stx in _import:
+        module, _, obj_alias = imp_stx.partition(':')
+        obj, _, alias = obj_alias.partition(':')
+        if not obj:
+            globals()[alias or module] = importlib.import_module(module)
+        else:
+            _garb = importlib.import_module(module)
+            globals()[alias or obj] = getattr(_garb, obj)
 
     if _file:
         _ = flu(read_file(_file)).map(str.rstrip)
