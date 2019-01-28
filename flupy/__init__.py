9507255be5e692a00a60d84138da6f4db34a0acc
Oliver Rice <github@oliverrice.com>
2019-01-28 08:45:17 -0600

    walk_files default abspath

--- flupy/__init__.py
+++ flupy/__init__.py
@@ -1,6 +1,6 @@
-__project__ = 'flupy'
+__project__ = "flupy"
 
-__version__ = '1.0.4'
+__version__ = "1.0.5"
 
 from flupy.fluent import flu, with_iter, Fluent
 from flupy.cli.utils import walk_files, walk_dirs
--- flupy/cli/utils.py
+++ flupy/cli/utils.py
@@ -4,17 +4,26 @@ from typing import Iterable
 from flupy import flu
 
 
-def walk_files(path: str = ".") -> Iterable[str]:
+def walk_files(path: str = ".", abspath=True) -> Iterable[str]:
     """Yield files recursively starting from *path"""
+
     def _impl():
         for d, dirs, files in os.walk(path):
             for x in files:
-                yield os.path.join(d, x)
+                rel_path = os.path.join(d, x)
+                if abspath:
+                    yield os.path.abspath(rel_path)
+                else:
+                    yield rel_path
+
     return flu(_impl())
+
 
 def walk_dirs(path: str = ".") -> Iterable[str]:
     """Yield files recursively starting from *path"""
+
     def _impl():
         for d, _, _ in os.walk(path):
             yield d
+
     return flu(_impl())
