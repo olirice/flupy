4326134c9ed74ed45f600c85e9dbb1bc875daccf
Oliver Rice <github@oliverrice.com>
2019-01-30 08:55:25 -0600

    multiple walk_path pathes

--- flupy/__init__.py
+++ flupy/__init__.py
@@ -1,6 +1,6 @@
 __project__ = "flupy"
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 
 from flupy.fluent import flu, with_iter, Fluent
 from flupy.cli.utils import walk_files, walk_dirs
--- flupy/cli/utils.py
+++ flupy/cli/utils.py
@@ -1,20 +1,24 @@
+# pylint: disable=invalid-name
 import os
 from typing import Iterable
 
 from flupy import flu
 
 
-def walk_files(path: str = ".", abspath=True) -> Iterable[str]:
-    """Yield files recursively starting from *path"""
+def walk_files(*pathes: str, abspath=True) -> Iterable[str]:
+    """Yield files recursively starting from each location in *pathes"""
+    if pathes == ():
+        pathes = (".",)
 
     def _impl():
-        for d, dirs, files in os.walk(path):
-            for x in files:
-                rel_path = os.path.join(d, x)
-                if abspath:
-                    yield os.path.abspath(rel_path)
-                else:
-                    yield rel_path
+        for path in pathes:
+            for d, _, files in os.walk(path):
+                for x in files:
+                    rel_path = os.path.join(d, x)
+                    if abspath:
+                        yield os.path.abspath(rel_path)
+                    else:
+                        yield rel_path
 
     return flu(_impl())
 
