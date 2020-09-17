03005cf3254068b0657c33f47ff546e4658a1064
Oliver Rice <github@oliverrice.com>
2020-09-17 06:10:07 -0500

    full test coverage

--- flupy/cli/cli.py
+++ flupy/cli/cli.py
@@ -1,9 +1,8 @@
 import argparse
 import importlib
 import sys
-from pathlib import Path
 from signal import SIG_DFL, SIGPIPE, signal
-from typing import List
+from typing import List, Optional
 
 from flupy import __version__, flu
 
@@ -49,9 +48,9 @@ def execute_imports(imps: List[str]):
             globals()[alias or obj] = getattr(_garb, obj)
 
 
-def main():
+def main(argv: Optional[List[str]] = None):
     """CLI Entrypoint"""
-    args = parse_args(sys.argv[1:])
+    args = parse_args(argv[1:] if argv is not None else sys.argv[1:])
 
     _command = args.command
     _file = args.file
@@ -61,51 +60,6 @@ def main():
 
     if _file:
         _ = flu(read_file(_file)).map(str.rstrip)
-    else:
-        # Do not raise exception for Broken Pipe
-        signal(SIGPIPE, SIG_DFL)
-        _ = flu(sys.stdin).map(str.rstrip)
-
-    pipeline = eval(_command)
-
-    if hasattr(pipeline, "__iter__") and not isinstance(pipeline, (str, bytes)):
-        for r in pipeline:
-            sys.stdout.write(str(r) + "\n")
-
-    elif pipeline is None:
-        pass
-    else:
-        sys.stdout.write(str(pipeline) + "\n")
-
-
-def precommit():
-    """Secondary entrypoing for pre-commit hook to handle multiple files
-    as positional arguments
-
-    For internal use only
-    """
-
-    def precommit_parse_args(args: List[str]):
-        parser = argparse.ArgumentParser(
-            description="flupy: a fluent interface for python", formatter_class=argparse.RawTextHelpFormatter
-        )
-        parser.add_argument("files", type=str, nargs="+", help="file pathes")
-        parser.add_argument("--command", help="command to execute against input")
-        parser.add_argument("-i", "--import", nargs="*", default=[])
-        return parser.parse_args(args)
-
-    args = precommit_parse_args(sys.argv[1:])
-
-    # Pull command from
-    _command = args.command
-    _files = args.files
-    _import = getattr(args, "import")
-
-    execute_imports(_import)
-
-    if _files:
-
-        _ = flu(_files).map(Path).filter(lambda x: x.is_file())
     else:
         # Do not raise exception for Broken Pipe
         signal(SIGPIPE, SIG_DFL)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,9 @@
 import sys
+from tempfile import NamedTemporaryFile
 
 import pytest
 
-from flupy.cli.cli import execute_imports, parse_args
+from flupy.cli.cli import execute_imports, main, parse_args
 
 
 def test_parse_args():
@@ -22,3 +23,59 @@ def test_parse_args():
 
     execute_imports(["json"])
     assert "json" in sys.modules
+
+
+def test_show_help(capsys):
+    with pytest.raises(SystemExit):
+        main(["flu", "-h"])
+
+    result = capsys.readouterr()
+    stdout = result.out
+    assert stdout.startswith("usage")
+
+
+def test_show_version(capsys):
+    main(["flu", "flu(range(5)).collect()"])
+
+    result = capsys.readouterr()
+    stdout = result.out.replace("\n", "")
+    assert stdout.startswith("0")
+
+
+def test_basic_pipeline(capsys):
+    main(["flu", "flu(range(5)).collect()"])
+    result = capsys.readouterr()
+    stdout = result.out.replace("\n", "")
+    assert stdout.startswith("0")
+
+
+def test_pass_on_none_pipeline(capsys):
+    main(["flu", "None"])
+    result = capsys.readouterr()
+    stdout = result.out
+    assert stdout == ""
+
+
+def test_non_iterable_non_none_pipeline(capsys):
+    main(["flu", '"hello_world"'])
+    result = capsys.readouterr()
+    stdout = result.out.strip("\n")
+    assert stdout == "hello_world"
+
+
+def test_from_file(capsys):
+    with NamedTemporaryFile("w+") as f:
+        f.write("hello")
+        f.read()
+        f_name = f.name
+        main(["flu", "-f", f_name, "_.map(str.upper)"])
+    result = capsys.readouterr()
+    stdout = result.out.strip("\n")
+    assert stdout == "HELLO"
+
+
+def test_glob_imports(capsys):
+    main(["flu", "flu(env).count()", "-i", "os:environ:env"])
+    result = capsys.readouterr()
+    stdout = result.out
+    assert stdout
